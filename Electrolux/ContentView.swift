315e20e7fff70f182ba20d7b5a41ecd688174924
Mehmet Can Seyhan <mehmet.can.seyhan1@gmail.com>
2021-07-11 15:04:32 +0200

    Initial Commit

---
+++ Electrolux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Electrolux
+//
+//  Created by Mehmet Can Seyhan on 2021-07-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Electrolux/ElectroluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectroluxApp.swift
+//  Electrolux
+//
+//  Created by Mehmet Can Seyhan on 2021-07-11.
+//
+
+import SwiftUI
+
+@main
+struct ElectroluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
